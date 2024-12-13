d3a49d1c964c350b6c92e8f68985fdeabc17cb1b
anaserrami <ramianas314@gmail.com>
2024-12-13 22:14:38 +0100

    add some products for test

--- inventory-service/src/main/java/com/errami/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/errami/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.errami.inventoryservice;
 
+import com.errami.inventoryservice.entities.Product;
+import com.errami.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,20 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository) {
+        return args -> {
+            for (int i = 0; i < 10; i++) {
+                Product product = Product.builder()
+                        .name("Product" + i)
+                        .price(Math.random() * 1000)
+                        .quantity((int) (Math.random() * 50))
+                        .build();
+                productRepository.save(product);
+            }
+            productRepository.findAll().forEach(product -> {
+                System.out.println(product.toString());
+            });
+        };
+    }
 }
