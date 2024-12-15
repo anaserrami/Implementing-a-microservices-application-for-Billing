9795a33ea09400d340702176c63ed8246269e288
anaserrami <ramianas314@gmail.com>
2024-12-15 18:42:07 +0100

    update customer repository

--- customer-service/src/main/java/com/errami/customerservice/repositories/CustomerRepository.java
+++ customer-service/src/main/java/com/errami/customerservice/repositories/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.errami.customerservice.repositories;
 
 import com.errami.customerservice.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CustomerRepository extends JpaRepository<Customer, String> {
 }
